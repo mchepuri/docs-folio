b8feacfb0f443ce9fe6967faeecbd1417bfd8863
mchepuri <mchepuri@schC02FK1PTMD6R.local>
2022-05-29 13:56:29 -0700

    Added express server to serve prod build

---
+++ scripts/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const port = process.env.PORT | 5000;
+
+if(process.env.NODE_ENV==='production')
+{
+    console.log('Prodution***');
+    app.use(express.static(path.resolve(__dirname,'../dist')));
+    app.get('/',(req,res)=>{
+        const fileWithPath=path.resolve(__dirname,'../dist/','index.html');
+        console.log('path ',fileWithPath);
+        res.sendFile( fileWithPath,function (err) {
+            if (err) {
+                next(err);
+            } else {
+                console.log('Sent:', fileWithPath);
+            }
+        });
+    });
+}
+
+app.listen(port,(err)=>{
+    if(err) return console.log(err);
+    console.log(' Server running on port ',port);
+});
