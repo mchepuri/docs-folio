7841c7c2413e74add5172331b2a2b97d31dd77da
mchepuri <mchepuri@schC02FK1PTMD6R.local>
2022-05-29 14:47:09 -0700

    Changed title to ObGyn instead of just gyn

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@
 
 export const headerData = {
     name: 'Dr Sindhusha Gundlapally',
-    title: "Gynecologist",
+    title: "Obstetrician & Gynecologist",
     desciption:"I chose a career in obstetrics and gynecology because there's something about honoring women, honoring the birth process. We all come from Women and there is something extra ordinary about women",
     image: 'https://babycatcher-static.s3.ap-south-1.amazonaws.com/imgs/Sindhu_Portfolio-Pic-4.jpg'
 }
